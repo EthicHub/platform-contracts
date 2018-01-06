7786b9af3f874602eb12e0891fd95d3f1dd12391
kkx <qijiongjiang1@hotmail.com>
2018-01-06 02:55:55 +0100

    add contract deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,16 @@
+const Lending = artifacts.require('Lending');
 
 module.exports = function(deployer) {
+    //01/10/2018
+    fundingStartTime = 1515542400;
+    //01/20/2018
+    fundingEndTime = 1516406400;
+    lendingInterestRatePercentage = 115;
+    // 3 eths
+    totalLendingAmount = 3000000000000000000;
+    //400 pesos per eth
+    initialEthPerFiatRate = 400;
+    lendingDays = 90;
+
+    deployer.deploy(Lending, fundingStartTime, fundingEndTime, '0x4a42CEAad068a8C926E19231d149ed3c1238E51F', lendingInterestRatePercentage, totalLendingAmount, initialEthPerFiatRate, lendingDays)
 };
--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: "192.168.99.100",
+      host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
     }
