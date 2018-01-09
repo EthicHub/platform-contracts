9962517d8175e047b2c7dddf6145f6e5b1b63df0
Raul <raul.mf86@gmail.com>
2018-01-09 01:10:49 +0100

    added script to return money

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,31 @@
 const Lending = artifacts.require('Lending');
 
+function latestTime() {
+  return web3.eth.getBlock(web3.eth.blockNumber).timestamp;
+}
+const duration = {
+  seconds: function (val) { return val },
+  minutes: function (val) { return val * this.seconds(60) },
+  hours: function (val) { return val * this.minutes(60) },
+  days: function (val) { return val * this.hours(24) },
+  weeks: function (val) { return val * this.days(7) },
+  years: function (val) { return val * this.days(365) }
+};
+
+function ether(n) {
+  return new web3.BigNumber(web3.toWei(n, 'ether'))
+}
+
+function now() {
+  return Math.round((new Date()).getTime() / 1000);
+}
+
+
 module.exports = function(deployer) {
     //01/10/2018
-    fundingStartTime = 1515542400;
+    fundingStartTime = now() + duration.minutes(15);
     //01/20/2018
-    fundingEndTime = 1516406400;
+    fundingEndTime = fundingStartTime + duration.days(1);
     lendingInterestRatePercentage = 115;
     // 3 eths
     totalLendingAmount = 3000000000000000000;
