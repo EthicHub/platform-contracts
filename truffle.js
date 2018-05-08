e77cf0fed9b35a6c1d391427f23a88c852c750d9
Ramarti <raul@MacBook-Pro-de-Raul.local>
2018-05-08 19:31:31 +0200

    deploy in rinkeby

--- truffle.js
+++ truffle.js
@@ -1,5 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+let mnemonic = process.env.MNEMONIC;
 
 module.exports = {
   solc: {
@@ -20,7 +25,15 @@ module.exports = {
         port: 9545,
         gas: 4600000,
         network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"+process.env.INFURA_KEY)
+      },
+      network_id: '*',
+      gasLimit: 6000000,
+      gas: 4700000
+    },
 
   }
 };
