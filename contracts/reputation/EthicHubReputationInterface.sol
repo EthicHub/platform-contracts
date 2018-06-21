e308b50e5e03f45f360b9cee87dcdfff01f72940
Ramarti <raul.mf86@gmail.com>
2018-06-21 11:38:58 +0200

    Warnings reputation

--- contracts/reputation/EthicHubReputationInterface.sol
+++ contracts/reputation/EthicHubReputationInterface.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.4.20;
 contract EthicHubReputationInterface {
     modifier onlyUsersContract(){_;}
     modifier onlyLendingContract(){_;}
-    function burnReputation(uint delayDays) onlyLendingContract external;
-    function incrementReputation(uint completedProjectsByTier) onlyLendingContract external;
-    function initLocalNodeReputation(address localNode) onlyUsersContract external;
-    function initCommunityReputation(address community) onlyUsersContract external;
+    function burnReputation(uint delayDays)  external;
+    function incrementReputation(uint completedProjectsByTier)  external;
+    function initLocalNodeReputation(address localNode)  external;
+    function initCommunityReputation(address community)  external;
     function getCommunityReputation(address target) public view returns(uint256);
     function getLocalNodeReputation(address target) public view returns(uint256);
 }
